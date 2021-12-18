676546a80b6b16e1f9bba6ca7b834c4d6e0feabc
bcdavis1990 <bcdavis1990@yahoo.com>
2021-12-18 17:24:03 -0500

    Test functions

--- js/script.js
+++ js/script.js
@@ -10,20 +10,34 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  { quote: 'test', author: 'test', citation: 'test', year: 'test'},
+  { quote: 'test2', author: 'test2',citation: 'test2', year: 'test2' },
+  { quote: 'test3', author: 'test3', citation: 'test3', year: 'test3'},
+  { quote: 'test4', author: 'test4', citation: 'test4', year: 'test4'},
+  { quote: 'test5', author: 'test5', citation: 'test5', year: 'test5'}
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
-
-
+function getRandomQuote() {
+  let randomNumber = Math.floor( Math.random() * (quotes.length));
+    return quotes[randomNumber];
+};
 
 /***
  * `printQuote` function
 ***/
-
-
+function printQuote () {
+  let html = '';
+  let randomQuote = getRandomQuote();
+  html +=
+  `<p class="quote">${randomQuote.quote}</p>
+  <p class="source">${randomQuote.author}<span class="citation">${randomQuote.citation}</span>
+  <span class="year">${randomQuote.year}</span></p>`
+};
 
 /***
  * click event listener for the print quote button
